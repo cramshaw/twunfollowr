a26e6d2445415bc71c605488856768fb8d08626a
cramshaw <chris.ramshaw@gmail.com>
2016-01-25 16:35:46 +0000

    fully working?

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ TWITTER_CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
 TWITTER_ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
 TWITTER_ACCESS_TOKEN_SECRET = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
-
 class Followr:
 
     def __init__(self, target):
@@ -22,7 +21,8 @@ class Followr:
         self.followers = list()
         self.following = list()
         self.followed = list()
-        self.recently_followed = self.load_last_followed()
+        self.failed_follows = list()
+        self.recently_followed = list()
         self.count = 0
 
     def make_auth(self):
@@ -31,23 +31,26 @@ class Followr:
         return auth
 
     def run(self):
-        self.remove_useless()
+        self.load_last_followed()
         self.get_current_followers()
         self.get_current_following()
+        self.remove_useless()
         self.follow()
         self.save_changes()
 
     def get_current_followers(self):
         self.followers = self.api.followers_ids()
+        print(self.followers)
 
     def get_current_following(self):
         self.following = self.api.friends_ids()
+        print(self.following)
 
     def follow(self):
         try:
             for target in self.api.followers_ids(screen_name=self.target):
                 if self.count < 50:
-                    if target not in self.followers and target not in self.following:
+                    if target not in self.followers and target not in self.following and target not in self.recently_followed and target not in self.failed_follows and target != self.api.me().id:
                         print('Following', target)
                         self.api.create_friendship(target)
                         sleep(randint(0,50))
@@ -63,18 +66,25 @@ class Followr:
     def save_changes(self):
         with open('follow.txt', 'wb') as fhand:
             pickle.dump(self.followed, fhand)
-            print(self.followed)
+            print('saved follows')
+        with open('failed_follows.txt', 'wb') as fhand:
+            pickle.dump(self.failed_follows, fhand)
+            print('saved failed follows')
+
 
     def load_last_followed(self):
         with open('follow.txt', 'rb') as fhand:
-            x = pickle.load(fhand)
-            print(x)
-            return x
+            self.recently_followed = pickle.load(fhand)
+        with open('failed_follows.txt', 'rb') as fhand:
+            self.failed_follows = pickle.load(fhand)
 
     def remove_useless(self):
         for user in self.recently_followed:
             if user not in self.followers:
+                self.failed_follows.append(user)
+                sleep(randint(0,50))
                 self.api.destroy_friendship(user)
+                print('unfollowed', user)
 
 if __name__ == "__main__":
     Followr("SkyBetChamp").run()
